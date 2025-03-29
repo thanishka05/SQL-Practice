9d86241b1451cceaf4b44a4b882c3b897e20bb8a
thanishka05 <thanishkareddy05@gmail.com>
2025-03-29 17:04:10 +0530

    This file as Group by and Having queries

---
+++ Groupby And Having.sql
@@ -0,0 +1,10 @@
+Basic GROUP BY Questions
+List the total number of students in each city.
+
+Count the number of male and female students (gender) separately.
+
+Find the total number of enrollments for each course.
+
+Display the number of students born in each year.
+
+Show how many students have registered for each course (cou_id).
